3f960f14e50ad1741f095aef44648f02345bc04a
Gabriel Morin <gabriel.morin@epita.fr>
2016-01-13 15:26:06 +0100

    Bootstrap

--- app/app.ts
+++ app/app.ts
@@ -1,15 +1,37 @@
-import {bootstrap} from 'angular2/platform/browser' ;
-import {Component} from 'angular2/core' ;
+import {bootstrap} from 'angular2/platform/browser';
+import {Component} from 'angular2/core';
 
-@Component ({
-	   selector : 'app' ,
-	   template : '<h1>Hello {{ name }}!</h1>'})
+@Component({
+  selector: 'product',
+  template: `
+  <h1>{{ id }}</h1>
+  <ul>
+      <li>
+      Name : {{name}}
+    </li>
+    <li>
+      Description : {{description}}
+    </li>
+  </ul>
 
-class AppComponent {
-      name : string ;
-      constructor() {
-      this.name = 'Angular 2' ;
-      }
+    <div><label>id: </label>{{id}}</div>
+    <div>
+      <label>name: </label>
+      <div><input [(ngModel)]="name" placeholder="name"></div>
+    </div>
+  `
+})
+class Product {
+  id: number;
+  name: string;
+  description: string;
+
+  constructor() {
+    this.id = 1;
+    this.name = 'Angular 2';
+    this.description = 'Super Framework'
+  }
 }
 
-bootstrap(AppComponent);
+
+bootstrap(Product);
